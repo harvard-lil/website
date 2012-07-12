642ce6fb4a2fc8f25988bfe8e732ebb5107b321c
Annie Cain <anniejocain@gmail.com>
2012-07-12 15:16:22 -0400

    projects page built with ingredients

---
+++ js/projects.js
@@ -0,0 +1,26 @@
+var bigProjectCount = 1,
+smallProjectCount = 1;
+
+$(function () { 
+	$('body').delegate('.description', 'click', function(e) {
+			var link = $(this).find('a').attr('href');
+			window.location = link;
+			e.preventDefault();
+		});
+  
+	$.getJSON('js/ingredients.json', function(data) {
+    $.each(data.projects.big, function(){
+      var source = $("#big-template").html();
+		  var template = Handlebars.compile(source);
+      $('#biggerProject' + bigProjectCount).html(template(this));
+      bigProjectCount++;
+    });
+    $.each(data.projects.small, function(){
+      var source = $("#big-template").html();
+		  var template = Handlebars.compile(source);
+      $('#smallerProject' + smallProjectCount).html(template(this));
+      smallProjectCount++;
+    });
+	});
+
+});
