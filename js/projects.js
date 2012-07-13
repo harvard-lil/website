dacc771f13dbfd4c281c47864bc2a0cc350a4a2e
Annie Cain <anniejocain@gmail.com>
2012-07-13 12:35:32 -0400

    hover images for project page

--- js/projects.js
+++ js/projects.js
@@ -3,10 +3,16 @@ smallProjectCount = 1;
 
 $(function () { 
 	$('body').delegate('.description', 'click', function(e) {
-			var link = $(this).find('a').attr('href');
-			window.location = link;
-			e.preventDefault();
-		});
+		var link = $(this).find('a').attr('href');
+		window.location = link;
+		e.preventDefault();
+	});
+	
+	$("body").on("mouseenter", ".hover", function(event) {
+		$(this).attr("src", $(this).attr('data-image-hover'));
+  }).on("mouseleave", ".hover", function(event) {
+    $(this).attr("src", $(this).attr('data-image'));
+  });
   
 	$.getJSON('js/ingredients.json', function(data) {
     $.each(data.projects.big, function(){
