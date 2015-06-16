b0ae40660ea15a61b8170fef96521be112f1d9bb
thisisdano <dan.o.williams@gmail.com>
2015-06-16 10:10:51 -0400

    rebuilt nav to be simpler and more consistent

--- theme/lil/javascripts/lilapp.js
+++ theme/lil/javascripts/lilapp.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 	var winHeight = $( window ).height();
-	$('header').height(winHeight);
+	var winWidth = $( window ).width();
+	var winMax = winWidth * 1.8;
+	if ( winHeight < winMax ) {
+		$('body.home header').height(winHeight);
+	} else {
+		$('body.home header').height(winMax);
+	}
 });
